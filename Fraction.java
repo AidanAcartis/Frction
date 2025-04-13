790b177fbc175b384d02453b53832d8868e2922e
AidanAcartis <ralaikotonadia@gmail.com>
2025-04-13 21:08:35 +0300

    Fraction

---
+++ Fraction.java
@@ -0,0 +1,82 @@
+public class Fraction {
+    private int numerator;
+    private int denominator;
+
+    public Fraction(int numerator, int denominator) {
+        if (denominator == 0) {
+            throw new ArithmeticException("Dénominateur ne peut pas être 0");
+        }
+        int gcd = gcd(Math.abs(numerator), Math.abs(denominator));
+        this.numerator = numerator / gcd;
+        this.denominator = denominator / gcd;
+        if (this.denominator < 0) {
+            this.numerator = -this.numerator;
+            this.denominator = -this.denominator;
+        }
+    }
+
+    // Méthodes utilitaires
+    private int gcd(int a, int b) {
+        if (b == 0) return a;
+        return gcd(b, a % b);
+    }
+
+    public Fraction add(Fraction other) {
+        if (this.denominator == other.denominator) {
+            return new Fraction(this.numerator + other.numerator, this.denominator);
+        } else {
+            int num = this.numerator * other.denominator + other.numerator * this.denominator;
+            int den = this.denominator * other.denominator;
+            return new Fraction(num, den);
+        }
+    }
+
+    public Fraction subtract(Fraction other) {
+        if (this.denominator == other.denominator) {
+            return new Fraction(this.numerator - other.numerator, this.denominator);
+        } else {
+            int num = this.numerator * other.denominator - other.numerator * this.denominator;
+            int den = this.denominator * other.denominator;
+            return new Fraction(num, den);
+        }
+    }
+
+     // Inverse d'une fraction
+     public Fraction inverse() {
+        if (this.numerator == 0) {
+            throw new ArithmeticException("Impossible d'inverser une fraction nulle");
+        }
+        return new Fraction(this.denominator, this.numerator);
+    }
+
+
+    public Fraction multiply(Fraction other) {
+        return new Fraction(this.numerator * other.numerator, this.denominator * other.denominator);
+    }
+
+    public Fraction divide(Fraction other) {
+        if (other.numerator == 0) {
+            throw new ArithmeticException("Division par zéro");
+        }
+        return new Fraction(this.numerator * other.denominator, this.denominator * other.numerator);
+    }
+
+    public boolean isZero() {
+        return this.numerator == 0;
+    }
+
+    @Override
+    public String toString() {
+        if (denominator == 1) return String.valueOf(numerator);
+        return numerator + "/" + denominator;
+    }
+
+    // Getters (si besoin)
+    public int getNumerator() {
+        return numerator;
+    }
+
+    public int getDenominator() {
+        return denominator;
+    }
+}
---
+++ FractionApp.java
@@ -0,0 +1,32 @@
+public class FractionApp {
+    public static void main(String[] args) {
+        // Créons quelques fractions
+        Fraction f1 = new Fraction(1, 2);   // 1/2
+        Fraction f2 = new Fraction(3, 4);   // 3/4
+        Fraction f3 = new Fraction(2, 4);   // 2/4 → doit être simplifié en 1/2
+
+        // Affichons-les
+        System.out.println("f1 = " + f1);
+        System.out.println("f2 = " + f2);
+        System.out.println("f3 = " + f3);
+
+        // Addition
+        Fraction sum = f1.add(f2);
+        System.out.println("f1 + f2 = " + sum);
+
+        // Soustraction
+        Fraction diff = f2.subtract(f1);
+        System.out.println("f2 - f1 = " + diff);
+
+        // Addition avec mêmes dénominateurs
+        Fraction sumSameDen = f1.add(f3);
+        System.out.println("f1 + f3 = " + sumSameDen);
+
+        // **Nouveau : Inverse**
+        Fraction inverseF1 = f1.inverse();
+        System.out.println("Inverse de f1 = " + inverseF1);
+
+        Fraction inverseF2 = f2.inverse();
+        System.out.println("Inverse de f2 = " + inverseF2);
+    }
+}
