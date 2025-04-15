8890960ef3674cca909c70630056637e3301cbdd
AidanAcartis <ralaikotonadia@gmail.com>
2025-04-15 14:09:54 +0300

    try 1

--- FractionApp.java
+++ FractionApp.java
@@ -4,11 +4,13 @@ public class FractionApp {
         Fraction f1 = new Fraction(1, 2);   // 1/2
         Fraction f2 = new Fraction(3, 4);   // 3/4
         Fraction f3 = new Fraction(2, 4);   // 2/4 → doit être simplifié en 1/2
+        Fraction f4 = new Fraction(3);
 
         // Affichons-les
         System.out.println("f1 = " + f1);
         System.out.println("f2 = " + f2);
         System.out.println("f3 = " + f3);
+        System.out.println("f4 = " + f4);
 
         // Addition
         Fraction sum = f1.add(f2);
@@ -21,6 +23,11 @@ public class FractionApp {
         // Addition avec mêmes dénominateurs
         Fraction sumSameDen = f1.add(f3);
         System.out.println("f1 + f3 = " + sumSameDen);
+
+        // Addition avec un entier
+        System.out.println("Here I am:");
+        Fraction sumWithEnter = f2.add(f4);
+        System.out.println("f2 + f4 = " + sumWithEnter);
 
         // **Nouveau : Inverse**
         Fraction inverseF1 = f1.inverse();
